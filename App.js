dbf8fd95998332afdff437e008199fef614ec943
longson <longson20062003@gmail.com>
2024-03-22 22:46:07 +0700

    update

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Wellcome to react-native expo </Text>
+      <Text>Wellcome to react-native expo  </Text>
       <StatusBar style="auto" />
     </View>
   );
