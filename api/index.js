974bb94b518ac39eff779594127f7af0a8fa01a5
longson <longson20062003@gmail.com>
2024-03-24 23:10:13 +0700

    design register and login

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const jwt = require("jsonwebtoken");
+
+mongoose
+  .connect(
+    "mongodb+srv://Longson2003s:sonn2003@cluster0.xv9rvsd.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("error connecting to MongoDB", err);
+  });
+
+app.listen(port,() => {
+    console.log("server is running on port 8000");
+});
